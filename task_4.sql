941374e7d78546da5dae5fbc748143e46b3cd3a8
Matherapyinfo <matherapyinfo@gmail.com>
2025-08-13 04:09:53 -0700

    Add task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,16 +1,16 @@
--- task_4.sql - Full description of books table without DESCRIBE/EXPLAIN
-
+-- task_4.sql - Books table description without DESCRIBE/EXPLAIN/ANALYZE
 SELECT 
     COLUMN_NAME AS 'Field',
     COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
+    IF(IS_NULLABLE = 'YES', 'YES', 'NO') AS 'Null',
+    IF(COLUMN_KEY = 'PRI', 'PRI', 
+       IF(COLUMN_KEY = 'MUL', 'MUL', '')) AS 'Key',
     COLUMN_DEFAULT AS 'Default',
     EXTRA AS 'Extra'
 FROM 
     INFORMATION_SCHEMA.COLUMNS 
 WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
+    TABLE_SCHEMA = DATABASE() 
     AND TABLE_NAME = 'books'
 ORDER BY 
     ORDINAL_POSITION;
